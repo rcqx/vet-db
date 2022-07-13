30061ef4fcb8c2f152081cbf505911037b830afe
jr-cast <castanos@pm.me>
2022-07-13 12:10:52 -0500

    Create owners and species id tables, update animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,49 @@ CREATE TABLE animals(
 --Add species column to existing animals table
 --ALTER TABLE animals
 --ADD species VARCHAR(100);
+
+--Create owners and species tables
+BEGIN;
+
+CREATE TABLE owners(
+    id SERIAL,
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+    id SERIAL,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+COMMIT;
+
+-- Modify animals table
+BEGIN;
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD id SERIAL PRIMARY KEY;
+
+COMMIT;
+
+-- Remove species columns, add species_id & owners_id columns
+BEGIN; 
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+COMMIT;
+
+
+
