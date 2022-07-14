29080130e1fbc40d496a6afa15c3f61ef07d6673
jr-cast <castanos@pm.me>
2022-07-14 12:40:50 -0500

    Add vets and specialization data

--- data.sql
+++ data.sql
@@ -88,6 +88,27 @@ WHERE name IN ('Angemon', 'Boarmon');
 
 COMMIT;
 
+-- INSERT VETS DATA
+BEGIN;
+
+ALTER SEQUENCE vets_id_seq RESTART WITH 1;
+
+INSERT INTO vets(name, age, date_of_graduation) VALUES('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Jack Harkness', 38, '2008-06-06');
+
+COMMIT;
+
+-- INSERT SPECIALIZATION DATA
+BEGIN;
+
+INSERT INTO specializations(vet_id, species_id) VALUES(1, 1);
+INSERT INTO specializations(vet_id, species_id) VALUES(3, 1);
+INSERT INTO specializations(vet_id, species_id) VALUES(3, 2);
+INSERT INTO specializations(vet_id, species_id) VALUES(4, 2);
+
+COMMIT;
 
 
 
